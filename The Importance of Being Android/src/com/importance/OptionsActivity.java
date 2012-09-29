15e51138c704d707684afb0224cdb9fbb8022534
Daniel Muir <danielmuir13@gmail.com>
2012-09-29 22:45:56 +0100

    Page spinner now updates dynamically

--- The Importance of Being Android/src/com/importance/OptionsActivity.java
+++ The Importance of Being Android/src/com/importance/OptionsActivity.java
@@ -21,10 +21,16 @@
 
 package com.importance;
 
-import com.example.android.R;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.android.R;
 
 /**
  * The Options Screen where the user can choose the part they want to select and
@@ -36,11 +42,53 @@ import android.os.Bundle;
  */
 
 public class OptionsActivity extends Activity {
-	
+
+	private Spinner mChar;
+	private Spinner mAct;
+	private Spinner mPage;
+	private ArrayAdapter<CharSequence> adapter;
+	private int resource;
+
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.options_layout);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.options_layout);
+
+		mChar = (Spinner) findViewById(R.id.spinnerCharacter);
+		mAct = (Spinner) findViewById(R.id.spinnerAct);
+		mPage = (Spinner) findViewById(R.id.spinnerPage);
+
+		mAct.setOnItemSelectedListener(new MyOnItemSelectedListener());
+
+	}
+
+	/**
+	 * This class updates the page spinner depending on the selection made in
+	 * the act spinner.
+	 * 
+	 * @author Dan
+	 * 
+	 */
+	public class MyOnItemSelectedListener implements OnItemSelectedListener {
+		public void onItemSelected(AdapterView<?> parent, View v, int pos,
+				long id) {
+			if (mAct.getSelectedItem().equals("Act I")) {
+				resource = R.array.act1_array;
+			} else if (mAct.getSelectedItem().equals("Act II")) {
+				resource = R.array.act2_array;
+			} else if (mAct.getSelectedItem().equals("Act III")) {
+				resource = R.array.act3_array;
+			}
+			adapter = ArrayAdapter.createFromResource(OptionsActivity.this,
+					resource, android.R.layout.simple_spinner_item);
+			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			mPage.setAdapter(adapter);
+		}
+
+		public void onNothingSelected(AdapterView<?> arg0) {
+			// Do nothing
+		}
+
 	}
 
 }
